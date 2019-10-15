3f083835cbae000332bc3298e08b9d92b91094e8
J <jmrumingan@gmail.com>
2019-10-15 14:15:36 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,7 +47,11 @@ def naming_system
       sub_category: {
         label: "Phylum",
           sub_category: {
-            label: "Class"
+            label: "Class",
+              sub_category: {
+                label: "Order"
+              }
+              
           }
       }
     
